1a12da439ba6598af18045a963120f78ea09fe5b
zohre <zohre78khedmati@gmail.com>
2023-01-17 19:47:28 +0330

    first commit

---
+++ src/test/java/com/rubic/cube/CubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rubic.cube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
